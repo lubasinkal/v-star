e271273c22ea080b4160f1d6c4067eb6dfc32f5a
lubasinkal <lubasinkal@outlook.com>
2026-03-18 13:27:14 +0200

    fix: generate 10M row dataset

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	f, _ := os.Create("2M_test.csv")
+	f, _ := os.Create("10M.csv")
 	defer f.Close()
 	fmt.Fprintln(f, "age,sex,policy_type,sum_assured,term")
-	for range 2000000 {
+	for range 10000000 {
 		age := 18 + rand.Intn(60)
 		sex := []string{"male", "female"}[rand.Intn(2)]
 		ptype := []string{"term", "whole_life", "endowment"}[rand.Intn(3)]
@@ -18,5 +18,5 @@ func main() {
 		term := 1 + rand.Intn(30)
 		fmt.Fprintf(f, "%d,%s,%s,%.2f,%d\n", age, sex, ptype, sum, term)
 	}
-	fmt.Println("Created 2M_test.csv with 2,000,000 rows")
+	fmt.Println("Created 10M.csv with 10,000,000 rows")
 }
